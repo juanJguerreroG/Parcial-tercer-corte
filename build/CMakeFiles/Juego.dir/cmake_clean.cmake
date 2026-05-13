b276d944645ae60aaacf333c2ea67a6dd74e6aed
Juan <juanjoguerreroxd@gmail.com>
2026-05-13 18:37:40 -0500

    .gitignore

---
+++ build/CMakeFiles/Juego.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Juego.dir/src/funciones.cpp.obj"
+  "CMakeFiles/Juego.dir/src/funciones.cpp.obj.d"
+  "CMakeFiles/Juego.dir/src/main.cpp.obj"
+  "CMakeFiles/Juego.dir/src/main.cpp.obj.d"
+  "Juego.exe"
+  "Juego.exe.manifest"
+  "Juego.pdb"
+  "libJuego.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Juego.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
